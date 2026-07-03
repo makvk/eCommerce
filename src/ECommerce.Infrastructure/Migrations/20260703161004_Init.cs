f66a868ec8860e325c271e5790c0d01b85e497f5
Виталий Гужавин <vitaliyguzhavin@gmail.com>
2026-07-03 19:46:29 +0300

    feat: setup postgres in docker and add initial ef core migrations

---
+++ src/ECommerce.Infrastructure/Migrations/20260703161004_Init.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "customer");
+
+            migrationBuilder.EnsureSchema(
+                name: "ordering");
+
+            migrationBuilder.EnsureSchema(
+                name: "catalog");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                schema: "customer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    PasswordHash = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    LastUpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Balance_Amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Balance_Currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    Name_FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Name_LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Name_MiddleName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastUpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Address_City = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Address_Country = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Address_PostalCode = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Address_Street = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "catalog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    StockQuantity = table.Column<int>(type: "integer", nullable: false),
+                    LastUpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Price_Amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Price_Currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    PriceAtPurchase_Amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    PriceAtPurchase_Currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Email",
+                schema: "customer",
+                table: "Customers",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                schema: "ordering",
+                table: "OrderItems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                schema: "ordering",
+                table: "Orders",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers",
+                schema: "customer");
+
+            migrationBuilder.DropTable(
+                name: "OrderItems",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "catalog");
+
+            migrationBuilder.DropTable(
+                name: "Orders",
+                schema: "ordering");
+        }
+    }
+}
---
+++ src/ECommerce.Infrastructure/Persistence/EDbContext.cs
@@ -0,0 +1,180 @@
+using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Persistence;
+
+public class EDbContext(DbContextOptions<EDbContext> options) : DbContext(options)
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.ToTable("Customers", "customer");
+            
+            entity.HasKey(e => e.Id);
+            
+            entity.Property(e => e.Email)
+                .IsRequired()
+                .HasMaxLength(150);
+            
+            entity.HasIndex(e => e.Email)
+                .IsUnique();
+            
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(255)
+                .IsRequired();
+            
+            entity.ComplexProperty(e => e.Name, builder =>
+            {
+                builder.Property(b => b.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+                
+                builder.Property(b => b.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+                
+                builder.Property(b => b.MiddleName)
+                    .HasMaxLength(50);
+            });
+
+            entity.ComplexProperty(e => e.Balance, builder =>
+            {
+                builder.Property(b => b.Amount)
+                    .IsRequired()
+                    .HasPrecision(18, 2);
+                
+                builder.Property(b => b.Currency)
+                    .IsRequired()
+                    .HasMaxLength(3);
+            });
+            
+            entity.Property(e => e.LastUpdatedAt)
+                .IsRequired()
+                .HasColumnType("timestamp with time zone");
+            
+            entity.Property(e => e.CreatedAt)
+                .IsRequired()
+                .HasColumnType("timestamp with time zone");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("Products", "catalog");
+            
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+            
+            entity.Property(e => e.Description)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            entity.ComplexProperty(e => e.Price, builder =>
+            {
+                builder.Property(b => b.Amount)
+                    .IsRequired()
+                    .HasPrecision(18, 2);
+                
+                builder.Property(b => b.Currency)
+                    .IsRequired()
+                    .HasMaxLength(3);
+            });
+
+            entity.Property(e => e.StockQuantity)
+                .IsRequired();
+            
+            entity.Property(e => e.LastUpdatedAt)
+                .IsRequired()
+                .HasColumnType("timestamp with time zone");
+            
+            entity.Property(e => e.CreatedAt)
+                .IsRequired()
+                .HasColumnType("timestamp with time zone");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.ToTable("Orders", "ordering");
+            entity.HasKey(e => e.Id);
+            
+            entity.HasIndex(e => e.CustomerId); 
+    
+            entity.Property(e => e.CustomerId)
+                .IsRequired();
+            
+            entity.ComplexProperty(e => e.Address, builder =>
+            {
+                builder.Property(b => b.City)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                builder.Property(b => b.Country)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                builder.Property(b => b.PostalCode)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                builder.Property(b => b.Street)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            entity.Property(e => e.Status)
+                .HasConversion<string>()
+                .HasMaxLength(50);
+
+            entity.Property(e => e.LastUpdatedAt)
+                .IsRequired()
+                .HasColumnType("timestamp with time zone");
+
+            entity.Property(e => e.CreatedAt)
+                .IsRequired()
+                .HasColumnType("timestamp with time zone");
+            
+            entity.HasMany(e => e.Items)
+                .WithOne()
+                .HasForeignKey(e => e.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+            
+            entity.Navigation(o => o.Items)
+                .Metadata.SetPropertyAccessMode(PropertyAccessMode.Field);
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.ToTable("OrderItems", "ordering");
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.OrderId)
+                .IsRequired();
+
+            entity.Property(ei => ei.ProductId)
+                .IsRequired();
+
+            entity.Property(ei => ei.Quantity)
+                .IsRequired();
+
+            entity.ComplexProperty(e => e.PriceAtPurchase, builder =>
+            {
+                builder.Property(b => b.Amount)
+                    .IsRequired()
+                    .HasPrecision(18, 2);
+
+                builder.Property(b => b.Currency)
+                    .IsRequired()
+                    .HasMaxLength(3);
+            });
+        });
+    }
+}
